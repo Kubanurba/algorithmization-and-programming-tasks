2f1e61f0b94f437406bddbdf533e2d17c5c184ca
Kubanurba <45840041+Kubanurba@users.noreply.github.com>
2019-09-14 15:14:58 +0300

    Update Code.cpp

--- Pract_4/Code.cpp
+++ Pract_4/Code.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	float x, y, p, a;
+
+	cout << "Input x: ";
+	cin >> x;
+	cout << "Input y: ";
+	cin >> y;
+	cout << "Input a: ";
+	cin >> a;
+	cout << "Input p: ";
+	cin >> p;
+
+	if (y < (a*x*x) - p)
+		cout << "The point is outside the parabola." << '\n';
+	else
+	{
+		if (y >= fabs(x))
+			cout << "The point is above the line.";
+		else
+			cout << "The point is in the area.";
+	}
+	_getch();
+}
 

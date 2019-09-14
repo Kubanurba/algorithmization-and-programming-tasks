698d837daa46a8154700535541860f0cce173577
Kubanurba <45840041+Kubanurba@users.noreply.github.com>
2019-09-14 15:20:54 +0300

    Update Code.cpp

--- Pract_7/Code.cpp
+++ Pract_7/Code.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+
+int number_of_1pairs(unsigned int x)
+{
+	int pair = 0;
+
+	for (; x > 0; x = x >> 1)
+	{
+		if (x & (x >> 1) & 1)
+			pair++;
+		
+	}
+	return pair;
+}
+
+
+
+int main()
+{
+	unsigned int x;
+	cout << "Enter your number x: ";
+	cin >> x;
+
+	cout << "Binary value is : " << bitset <32>(x) << "\nNumber of 1 pairs is : "  << number_of_1pairs(x) << endl;
+
+}
 

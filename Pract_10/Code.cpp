2e96813a7aecc9ce6c79bc4c58f6049177ef3ff8
Kubanurba <45840041+Kubanurba@users.noreply.github.com>
2019-09-14 15:23:31 +0300

    Update Code.cpp

--- Pract_10/Code.cpp
+++ Pract_10/Code.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void Input(int &n, int array[])
+	{
+		
+		for (int i = 0; i < n; i++)
+		{
+			cout << "Input a[" << i << "] = ";
+			cin >> array[i];
+		}
+	}
+
+
+void Process(int n, int *array)
+	{
+		int max = array[0];
+		for (int i = 0; i < n; i++)
+		{
+			if (array[i] > max)
+			{
+				max = array[i];
+				for (int j = 0; j <= i; j++)
+				{
+					if (array[j] < 0)
+						array[j] = 0;
+				}
+			}
+		}
+	}
+
+
+void Output(int *array, int n)
+{
+	cout << "New array -->  ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+
+int main()
+{
+	int n;
+	cout << "Array size = ";
+	cin >> n;
+	int *array = new int[n];
+
+	Input(n, array);
+	Process(n, array);
+	Output(array, n);
+
+	delete[]array;
+	_getch();
+	return 0;
+}
+
 

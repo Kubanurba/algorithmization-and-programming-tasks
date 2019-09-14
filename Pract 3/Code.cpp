d2e080ab2a0360e64e05a928eb9f8e21e2e60802
Kubanurba <45840041+Kubanurba@users.noreply.github.com>
2019-09-14 15:04:53 +0300

    Update Code.cpp

--- Pract 3/Code.cpp
+++ Pract 3/Code.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+int main()
+{
+	float a(-1.35), b(1.45), c(5.15), d(-2.22);
+
+	float y = (2 * tan(b) / log(fabs(2 * a)) - sqrt((sqrt(fabs(d) / c))));
+
+	cout << "a=" << a << '\n' << "b=" << b << '\n' << "c=" << c << '\n' << "d=" << d << '\n' << "y=" << y << '\n';
+
+	system("pause");
+	return 0;
+}

e771ceef4786d8e608d9460e38420639a146fe42
Kubanurba <45840041+Kubanurba@users.noreply.github.com>
2019-09-14 15:20:18 +0300

    Update Code.cpp

--- Pract_6/Code.cpp
+++ Pract_6/Code.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int m;
+	cout << "Your number is : ";
+	cin >> m;
+	int i = 2;
+	
+	for (; i < m && m % i; i++);
+	if (i == m)
+		cout << "The number you`ve entered is prime.";
+	else
+	{
+		cout << "The number you`ve entered is not prime."<<endl;
+		cout << "Multipliers are : ";
+		int i = 2;
+		while (i < m)
+		{
+			if (m%i == 0)
+			{
+				cout << i << "*";
+				m = m / i;
+			}
+			else
+			{
+				i++;
+			}
+		}
+		cout << m << endl;
+	}
+	
+	return 0;
+}
 
